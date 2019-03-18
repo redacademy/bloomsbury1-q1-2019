f6ebe0799ce66f4d00f593d34db2ff2b774c9ede
sindatam <lilli.sinda.tam@gmail.com>
2019-03-18 16:13:32 +0000

    Hamburger slide in menu with updated subjects, fixed navbar, login as a button along with the navbar color

--- themes/bloomsbury-theme/header.php
+++ themes/bloomsbury-theme/header.php
@@ -5,56 +5,119 @@
  * @package Bloomsbury_Theme
  */
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
-	</head>
+</head>
 
-	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<a class="skip-link screen-reader-text" href="#content">
+			<?php echo esc_html( 'Skip to content' ); ?></a>
 
-			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
+		<header id="masthead" class="site-header" role="banner">
+			<div class="site-branding">
+
+
 				<!-- Hamburger menu and navigation bar -->
-					<div class="collapse-menu">
-						<div id="menuToggle">
+				<div class="collapse-menu">
+					<div id="menuToggle">
 
-							<input type="checkbox" />
+						<input type="checkbox" />
 
-							<span></span>
-							<span></span>
-							<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
 
-							<ul id="menu">
-								<a href="#"><li>Home</li></a>
-								<a href="#"><li>About</li></a>
-								<a href="#"><li>Courses</li></a>
-								<a href="#"><li>Events</li></a>
-								<a href="#"><li>Parents' Area</li></a>
-								<a href="#"><li>Members' Area</li></a>
-								<a href="#"><li>Support Us</li></a>
-								<a href="#"><li>Contact Us</li></a>
-								<a href="#"><li>Log Out</li></a>
-							</ul>
-  						</div>
+						<ul id="menu">
+							<div class="profile-container">
+								<div class="profile-pic"></div>
+							</div>
+
+							<div class="personal-info">
+								<p>Fullname</p>
+								<p>Job Title</p>
+							</div>
+
+							<!-- Meny for the slidebar and sub-section for the Member's area -->
+							<div class="slidebar-menu">
+								<a href="#">
+									<li><i class="fas fa-home"></i>&nbsp; Home</li>
+								</a>
+								<hr>
+								<a href="#">
+									<li><i class="fas fa-book-open"></i>&nbsp; About</li>
+								</a>
+								<hr>
+								<a href="#">
+									<li><i class="fas fa-graduation-cap"></i>&nbsp; Courses</li>
+								</a>
+								<hr>
+								<a href="#">
+									<li><i class="far fa-calendar-check"></i>&nbsp; Events</li>
+								</a>
+								<hr>
+								<a href="#">
+									<li><i class="fas fa-key"></i>&nbsp; Members' Area</li>
+								</a>
+								<hr>
+
+								<div class="member-area-subsection">
+									<a href="#">
+										<li>Dashboard</li>
+									</a>
+									<a href="#">
+										<li>Profile</li>
+									</a>
+									<a href="#">
+										<li>Lean Canvas</li>
+									</a>
+									<a href="#">
+										<li>Community</li>
+									</a>
+									<a href="#">
+										<li>Calendar</li>
+									</a>
+									<a href="#">
+										<li>Resources</li>
+									</a>
+								</div>
+								<a href="#">
+									<li><i class="far fa-handshake"></i></i>&nbsp; Support Us</li>
+								</a>
+								<hr>
+								<a href="#">
+									<li><i class="fas fa-phone-volume"></i>&nbsp; Contact Us</li>
+								</a>
+								<hr>
+								<a href="#">
+									<li><i class="fas fa-sign-out-alt"></i>&nbsp; Log Out</li>
+								</a>
+								<hr>
+							</div>
+
+
+						</ul>
 					</div>
-
+				</div>
 				<!-- Bloomsbury Beginnings logo -->
 				<img class="bb-logo" src="<?php echo get_stylesheet_directory_uri() . "/bb-logo.png"; ?>">
 
+				<!-- Login button in the navbar -->
+
 				<div class="login-member-area">
-					<p>Login</p>
+					<button class="login-button">Login</button>
 				</div>
 
+			</div><!-- .site-branding -->
+		</header>
 
-				</div><!-- .site-branding -->
-				</header>
-
-			<div id="content" class="site-content">
+		<div id="content" class="site-content">
