b47da13f912b9d64354c13fa0f44d529f962f952
sindatam <lilli.sinda.tam@gmail.com>
2019-03-20 16:10:48 +0000

    Worked on indentetion on footer.php

--- themes/bloomsbury-theme/footer.php
+++ themes/bloomsbury-theme/footer.php
@@ -4,40 +4,38 @@
  *
  * @package Bloomsbury_Theme
  */
-
 ?>
-			</div><!-- #content -->
-
+	</div><!-- #content -->
 
 			<!-- footer -->
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
 					
-				<div class="footer-title">
-					<p>Bloomsbury Beginnings</p>
-					</div>
+	<div class="footer-title">
+		<p>Bloomsbury Beginnings</p>
+	</div>
 
-				<div class="social-info">
-				<div class="social-links">
-						<p>Home</p>
-						<p>Privacy</p>
-						<p>Contact</p>
-						<p>Blog</p>
-				</div>
-				
+	<div class="social-info">
+		<div class="social-links">
+				<p>Home</p>
+				<p>Privacy</p>
+				<p>Contact</p>
+				<p>Blog</p>
+		</div>
+
 				<!-- social media logos -->
-				<div class="social-logos">
-				<i class="fab fa-twitter"></i>
-				<i class="fab fa-instagram"></i>
-				<i class="fab fa-linkedin"></i>
-				<i class="fab fa-facebook"></i>
-				</div>				
-				</div><!-- .site-info -->
+		<div class="social-logos">
+			<i class="fab fa-twitter"></i>
+			<i class="fab fa-instagram"></i>
+			<i class="fab fa-linkedin"></i>
+			<i class="fab fa-facebook"></i>
+		</div>				
+	</div><!-- .site-info -->
 
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 	</body>
 </html>
