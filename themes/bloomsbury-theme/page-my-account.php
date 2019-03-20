24f82f8d9df7820cb4167ba11d3d193d0f490edb
Oliver Heward <oliverhewardbie@gmail.com>
2019-03-20 16:16:00 +0000

    WIP

---
+++ themes/bloomsbury-theme/page-my-account.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package Bloomsbury_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+
+					<h2><?php echo $current_user->display_name ?></h2>
+
+					<!-- Entry Content is the Dashboard content -->
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+								'after'  => '</div>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-## -->
+
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
