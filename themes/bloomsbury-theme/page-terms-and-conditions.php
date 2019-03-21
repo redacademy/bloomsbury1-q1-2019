a3633a46428d39e0088aaa0723f94394695ad038
ziyad <ukuniversity@hotmail.com>
2019-03-21 16:32:20 +0000

    Edited Images Size

--- themes/bloomsbury-theme/page-terms-and-conditions.php
+++ themes/bloomsbury-theme/page-terms-and-conditions.php
@@ -8,24 +8,24 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-        <!-- Start of page -->
-        <section class="header">
-            <!-- background linear-gradient(#01ADB5, #4a90e2) -->
+	<main id="main" class="site-main" role="main">
+			<!-- Start of page -->
+		<section class="header">
+			<!-- background linear-gradient(#01ADB5, #4a90e2) -->
             <div class="page-cover">
-                <h1 class="header-text">Terms & Conditions</h1>
+				<h1 class="header-text">Terms & Conditions</h1>
                 <p class="under-text">T&C Privacy</p>
             </div>
 		</section>
-            <!-- end of page -->
-			<?php while ( have_posts() ) : the_post(); ?>
-            
-            <?php get_template_part( 'template-parts/content', 'page' ); ?>
-            
-			<?php endwhile; // End of the loop. ?>
-            
-		</main><!-- #main -->
-	</div><!-- #primary -->
-    
-    <?php get_sidebar(); ?>
-    <?php get_footer(); ?>
+		<!-- end of page -->
+		<?php while ( have_posts() ) : the_post(); ?>
+		
+		<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		
+		<?php endwhile; // End of the loop. ?>
+		
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
