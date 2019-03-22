9214e24f54a451403f310ab2c4185463f19658e2
ziyad <ukuniversity@hotmail.com>
2019-03-22 16:46:08 +0000

    minor changes

--- themes/bloomsbury-theme/page-terms-and-conditions.php
+++ themes/bloomsbury-theme/page-terms-and-conditions.php
@@ -19,7 +19,7 @@ get_header(); ?>
 		</section>
 		<!-- end of page -->
 		<?php while ( have_posts() ) : the_post(); ?>
-		
+
 		<?php get_template_part( 'template-parts/content', 'page' ); ?>
 		
 		<?php endwhile; // End of the loop. ?>
