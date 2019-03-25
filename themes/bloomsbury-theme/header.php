8de365151e10b528dd4c873c051b2a375ac61c72
sindatam <lilli.sinda.tam@gmail.com>
2019-03-25 13:42:07 +0000

    Made amendments to the header.

--- themes/bloomsbury-theme/header.php
+++ themes/bloomsbury-theme/header.php
@@ -38,26 +38,20 @@
 						<span></span>
 
 						<ul id="menu">
-							<div class="profile-container">
-								<div class="profile-pic"></div>
-								
-<!-- <?php 
-//	global $current_user;
-//	if ( is_user_logged_in() );
-	//	get_currentuserinfo();
-	//	echo get_avatar( $current_user);
-//endif;
-?> -->
-</div>
-							<div class="personal-info">
-								<p>Fullname</p>
-								<p>Job Title</p>
 
+<!-- If user is logged in show the profile pic and profile info -->
 
-				</div>
+<?php if ( is_user_logged_in() ) { ?>
+		<div class="profile-container"></div>
+		<div class="profile-pic"></div> 
 
-							<!-- Menu for the slidebar and sub-section for the Member's area -->
-				<div class="slidebar-menu">
+		<div class="personal-info">
+			<p>Fullname</p>
+			<p>Job Title</p>
+		</div> 		
+
+<!-- Menu for the slidebar and sub-section for the Member's area -->
+						<div class="slidebar-menu">
 					<a href="#"><li><i class="fas fa-home"></i>&nbsp; Home</li></a>
 				<hr>
 					<a href="#"><li><i class="fas fa-book-open"></i>&nbsp; About</li></a>
@@ -69,9 +63,9 @@
 					<a href="#"><li><i class="fas fa-blog"></i>&nbsp; Blog</li></a>
 				<hr>
 					<a href="#"><li><i class="fas fa-key"></i>&nbsp; Members' Area</li></a>
-				<hr>
+				<hr> 
 
-					<div class="member-area-subsection">
+				<div class="member-area-subsection">
 						<a href="#"><li>Dashboard</li></a>
 						<a href="#"><li>Profile</li></a>
 						<a href="#"><li>Lean Canvas</li></a>
@@ -84,21 +78,52 @@
 					<hr>
 						<a href="#"><li><i class="fas fa-phone-volume"></i>&nbsp; Contact Us</li></a>
 					<hr>
-						<a href="#"><li><i class="fas fa-sign-out-alt"></i>&nbsp; Log Out</li></a>
+						<a href="#"><li><i class="fas fa-paste"></i></i>&nbsp; Terms and Conditions Privacy</li></a>
 					<hr>
+						<a href="#"><li><i class="fas fa-sign-out-alt"></i>&nbsp; Log Out</li></a>
+					<hr> 
 				</div>
 		</div>
+	</div>								
+<?php } else { ?>
+	<div class="slidebar-menu">
+				<a href="#"><li><i class="fas fa-home"></i>&nbsp; Home</li></a>
+				<hr>
+				<a href="#"><li><i class="fas fa-book-open"></i>&nbsp; About</li></a>
+				<hr>
+				<a href="#"><li><i class="fas fa-graduation-cap"></i>&nbsp; Courses</li></a>
+				<hr>
+				<a href="#"><li><i class="far fa-calendar-check"></i>&nbsp; Events</li></a>
+				<hr>
+				<a href="#"><li><i class="fas fa-blog"></i>&nbsp; Blog</li></a>
+				<hr>
+				<a href="#"><li><i class="far fa-handshake"></i></i>&nbsp; Support Us</li></a>
+				<hr>
+				<a href="#"><li><i class="fas fa-phone-volume"></i>&nbsp; Contact Us</li></a>
+				<hr>
+				<a href="#"><li><i class="fas fa-paste"></i></i>&nbsp; Terms and Conditions Privacy</li></a>
+				<hr>
 	</div>
+<?php } ?>
+	
 <!-- Bloomsbury Beginnings logo -->
 	<img class="bb-logo" src="<?php echo get_stylesheet_directory_uri() . "/bb-logo.png"; ?>">
 
 <!-- Login button in the navbar -->
 
+<?php if ( is_user_logged_in() ) { ?>
+
+	<div class="search-bar">
+	<input type="text" placeholder="Search.." id='search-text-input' />	
+	</div>
+
+<?php } else { ?>
 	<div class="login-member-area">
 		<button class="login-button">Login</button>
 	</div>
 
+<?php } ?>
+
 	</div><!-- .site-branding -->
 	</header>
 
-	<div id="content" class="site-content">
