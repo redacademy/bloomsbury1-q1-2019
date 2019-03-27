5bb09975c8cfc0d119bb5e9c24e97083040e6e0d
Oliver Heward <oliverhewardbie@gmail.com>
2019-03-27 00:10:58 +0000

    Fixed footer.php + __footer.scss. Need Brand SVG from Designers

--- themes/bloomsbury-theme/footer.php
+++ themes/bloomsbury-theme/footer.php
@@ -5,37 +5,56 @@
  * @package Bloomsbury_Theme
  */
 ?>
-	</div><!-- #content -->
+	</div>
+	<!-- End of Content -->
 
-			<!-- footer -->
+<!-- Start of Footer -->
 <footer id="colophon" class="site-footer" role="contentinfo">
+	<!-- Start of Site-Info -->
 	<div class="site-info">
 		
-	<div class="footer-title">
-		<p>Bloomsbury Beginnings</p>
-	</div>
-
-	<div class="social-info">
-		<div class="social-links">
-				<p>Home</p>
-				<p>Privacy</p>
-				<p>Contact</p>
-				<p>Blog</p>
+		<!-- Footer Brand Logo - ADD SVG!!  -->
+		<div class="footer-logo">
+			<p>Bloomsbury Beginnings Logo</p>
 		</div>
 
-				<!-- social media logos -->
-		<div class="social-logos">
-			<a href="https://twitter.com/bloomsbabies?lang=en" target="_blank"> <i class="fab fa-twitter"></i>
-			<a href="https://www.instagram.com/bloomsburybeginnings/" target="_blank"> <i class="fab fa-instagram"></i>
-			<a href="https://www.linkedin.com/company/bloomsbury-beginnings-cic" target="_blank"> <i class="fab fa-linkedin"></i>
-			<a href="https://www.facebook.com/bloomsburybeginnings/" target="_blank"> <i class="fab fa-facebook"></i>
-		</div>				
-	</div><!-- .site-info -->
+		<!-- Footer Page Links -->
+		<div class="footer-pages">
+			<a href="#"><h3 class="footer-title">Home</h3></a>
+			<a href="#"><h3 class="footer-title">Privacy</h3></a>
+			<a href="#"><h3 class="footer-title">Contact</h3></a>
+			<a href="#"><h3 class="footer-title">Blog</h3></a>
+		</div>
 
-</footer><!-- #colophon -->
-</div><!-- #page -->
+		<!-- Social Media Logos -->
+		<div class="social-logos">
+			<!-- Twitter -->
+			<a href="https://twitter.com/bloomsbabies?lang=en" target="_blank">
+				<i class="fab fa-twitter"></i>
+			</a>
+			<!-- Instagram -->
+			<a href="https://www.instagram.com/bloomsburybeginnings/" target="_blank"> 
+				<i class="fab fa-instagram"></i>
+			</a>
+			<!-- Linked-In -->
+			<a href="https://www.linkedin.com/company/bloomsbury-beginnings-cic" target="_blank"> 
+				<i class="fab fa-linkedin"></i>
+			</a>
+			<!-- Facebook -->
+			<a href="https://www.facebook.com/bloomsburybeginnings/" target="_blank"> 
+				<i class="fab fa-facebook"></i>
+			</a>
+		</div>
+		<!-- End of Social Media Logos -->				
+	</div>
+	<!-- End of Site-Info -->
+
+</footer>
+<!-- End of Site Footer -->
+</div>
+<!-- End of PAGE -->
 
 <?php wp_footer(); ?>
 
-	</body>
+</body>
 </html>
