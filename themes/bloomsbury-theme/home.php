c371f9579b97b894266f16b3e098467ea3248c08
Oliver Heward <oliverhewardbie@gmail.com>
2019-04-01 11:33:48 +0100

    Added Blog archive page under home.php and styled

--- themes/bloomsbury-theme/home.php
+++ themes/bloomsbury-theme/home.php
@@ -12,15 +12,14 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
+			<?php if ( is_home() ) : ?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
-			<?php
-			$backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
-			?>
-			<section class="blog-home-hero" stlye="background: url('<?php echo $backgroundImg[0] no-repeat; ?>')">
+
+			<section class="blog-home-hero">
+				<h1 class="blog-title">Blog</h1>
 			</section>
 
 			<?php /* Start the Loop */ ?>
@@ -32,21 +31,27 @@ get_header(); ?>
 							<?php the_post_thumbnail( 'large' ); ?>
 						<?php endif; ?>
 
-						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
 						<?php if ( 'post' === get_post_type() ) : ?>
 						<div class="entry-meta">
-							<?php bloomsbury_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php bloomsbury_posted_by(); ?>
+							<?php bloomsbury_posted_on(); ?> 
+							<?php bloomsbury_posted_by(); ?>
 						</div><!-- .entry-meta -->
 						<?php endif; ?>
+
+						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
 						<?php the_excerpt(); ?>
 					</div><!-- .entry-content -->
+
+					<div class="comment-meta">
+						<p class="blog-comments"><i class="fas fa-comment"></i> <?php comments_number( '0', '1', '%' ); ?></p>
+						<p class="blog-likes"><i class="fas fa-heart"></i> 0</p>
+						<p class="blog-share"><i class="fas fa-share-alt"></i> Share </p>
+					</div><!-- .entry-meta -->
 				</article><!-- #post-## -->
-
-
 
 			<?php endwhile; ?>
 
@@ -61,5 +66,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
